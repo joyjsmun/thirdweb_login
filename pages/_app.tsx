a4cd7075c4e2812458b290180a6a3074d2fe4390
joyjsmun <joy.js.mun@gmail.com>
2023-05-20 23:50:00 +0900

    ADD: supported chains

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { BaseGoerli,PolygonZkevmTestnet } from "@thirdweb-dev/chains";
+
 import "../styles/globals.css";
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+//const activeChain = "ethereum";
+const activeChain = "polygon"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider activeChain={BaseGoerli} supportedChains={[BaseGoerli,PolygonZkevmTestnet]}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
