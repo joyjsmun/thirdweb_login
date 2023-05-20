08fb7a995a8df113082fdd3bdd8a94377b7c60e0
joyjsmun <joy.js.mun@gmail.com>
2023-05-20 23:40:17 +0900

    ADD: first commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import { ConnectWallet } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <ConnectWallet />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
